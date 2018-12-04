057e8e32a4a0d3f48626092db3a890e555f5d11f
Robin Millette <robin@millette.info>
2018-12-04 03:18:19 -0500

    Add more links to main pages.

--- pages/page.js
+++ pages/page.js
@@ -1,1 +1,33 @@
-export default () => 'Hello Sam Man!'
+import Link from 'next/link'
+import { withRouter } from 'next/router'
+
+const Index = ({ router: { query }}) => <div>
+  <p>Hello Sam Man!</p>
+  <ul>
+    <li><Link href="/page" as="/"><a>Home page</a></Link></li>
+    <li><Link href="/page?page=page-2" as="/page-2"><a>Second page</a></Link></li>
+    <li><Link href="/page?page=page-3" as="/page-3"><a>Third page</a></Link></li>
+    <li><Link href="/page?page=page-5" as="/page-5"><a>404...</a></Link></li>
+    <li><Link href="/other"><a>Other page</a></Link></li>
+    <li><Link href="/about"><a>About page</a></Link></li>
+    <li><Link href="/contact"><a>Contact page</a></Link></li>
+  </ul>
+  {Object.keys(query).length > 0 && <>
+    <h2>Query</h2>
+    <pre>{JSON.stringify(query, null, '  ')}</pre>
+  </>}
+</div>
+
+Index.getInitialProps = async (o) => {
+  const keys = Object.keys(o)
+  console.log('OOO:', o)
+  console.log('OOO:', keys)
+  return { keys }
+  /*
+  const res = await fetch('https://api.github.com/repos/zeit/next.js')
+  const json = await res.json()
+  return { stars: json.stargazers_count }
+  */
+}
+
+export default withRouter(Index)
