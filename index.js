36ef495e06ebba6da10eea7968f66a1a3f14e4a7
Robin Millette <robin@millette.info>
2018-12-04 03:18:42 -0500

    Handle nextjs routes.

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const fastify = require('fastify')()
 
 const dev = process.env.NODE_ENV !== 'production'
 
+fastify.get('/favicon.ico', async (req, reply) => {
+  reply.code(404)
+  throw new Error('Niet')
+})
+
+const addCoreRoutes = (reserverd) => reserverd.forEach((p) => fastify.next(`/${p}`))
+
+// from the database
+const unknownPage = (p) => (['', 'page-2', 'page-3']).indexOf(p) === -1
+
 fastify
   .register(require('fastify-react'), { dev })
   .after(() => {
-    fastify.next('/')
+    // hardcoded, could be read from /pages/*.js or nextjs' API (tbd)
+    addCoreRoutes(['other', 'about', 'contact'])
+    fastify.next('/:page', async (app, { req, query, params: { page } }, reply) => {
+      if (unknownPage(page)) return app.render404(req, reply.res)
+      const html = await app.renderToHTML(req, reply.res, '/page', page ? { ...query, page } : {})
+      reply.type('text/html')
+      return html
+    })
   })
 
 fastify.listen(3000, process.env.HOSTNAME)
