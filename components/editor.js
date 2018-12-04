afb56eb31ebfce43c5610362da1d3c9b091a627e
Robin Millette <robin@millette.info>
2018-12-04 07:10:00 -0500

    Show html (with tags, etc.)

--- components/editor.js
+++ components/editor.js
@@ -1,11 +1,20 @@
+// npm
 import { Component } from 'react'
 import { Editor, EditorState, ContentState } from 'draft-js'
+import { stateToHTML } from 'draft-js-export-html'
 
 class MyEditor extends Component {
   constructor(props) {
     super(props)
-    this.state = { editorState: false }
-    this.onChange = (editorState) => this.setState({ editorState })
+    this.state = { editorState: false, html: false }
+    this.onChange = (editorState) => this.setState({ html: false, editorState })
+
+    this.showHTML = () => {
+      const html = stateToHTML(this.state.editorState.getCurrentContent())
+      console.log('HTML:', html)
+      console.log('HTML.length', html.length)
+      this.setState({ html })
+    }
   }
 
   componentDidMount() {
@@ -17,11 +26,22 @@ class MyEditor extends Component {
   }
 
   render() {
-    return this.state.editorState ? (
+    return (
+    <>
+    {this.state.editorState ? (
         <Editor editorKey={this.props.editorKey} editorState={this.state.editorState} onChange={this.onChange} />
     ) : (
       <div dangerouslySetInnerHTML={{ __html: this.props.initialContent }} />
-    )
+    )}
+    <button onClick={this.showHTML}>
+      Show updated html output
+    </button>
+
+    <div>
+    {this.state.html}
+    </div>
+        </>
+  )
   }
 }
 
