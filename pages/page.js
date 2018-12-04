739e6a17175a925dc30f67acfd9df9d8bc5b038e
Robin Millette <robin@millette.info>
2018-12-04 04:25:01 -0500

    Fetch page content thru api.

--- pages/page.js
+++ pages/page.js
@@ -1,33 +1,40 @@
+// npm
 import Link from 'next/link'
-import { withRouter } from 'next/router'
+import Error from 'next/error'
 
-const Index = ({ router: { query }}) => <div>
-  <p>Hello Sam Man!</p>
-  <ul>
-    <li><Link href="/page" as="/"><a>Home page</a></Link></li>
-    <li><Link href="/page?page=page-2" as="/page-2"><a>Second page</a></Link></li>
-    <li><Link href="/page?page=page-3" as="/page-3"><a>Third page</a></Link></li>
-    <li><Link href="/page?page=page-5" as="/page-5"><a>404...</a></Link></li>
-    <li><Link href="/other"><a>Other page</a></Link></li>
-    <li><Link href="/about"><a>About page</a></Link></li>
-    <li><Link href="/contact"><a>Contact page</a></Link></li>
-  </ul>
-  {Object.keys(query).length > 0 && <>
-    <h2>Query</h2>
-    <pre>{JSON.stringify(query, null, '  ')}</pre>
-  </>}
-</div>
+const Index = ({ json }) => {
+  // FIXME: should offer to create page
+  if (json.statusCode) return <Error statusCode={json.statusCode} />
 
-Index.getInitialProps = async (o) => {
-  const keys = Object.keys(o)
-  console.log('OOO:', o)
-  console.log('OOO:', keys)
-  return { keys }
-  /*
-  const res = await fetch('https://api.github.com/repos/zeit/next.js')
-  const json = await res.json()
-  return { stars: json.stargazers_count }
-  */
+  return (
+    <div>
+    <p>Hello Sam Man!</p>
+    <ul>
+      <li><Link href="/page" as="/"><a>Home page</a></Link></li>
+      <li><Link href="/page?page=page-2" as="/page-2"><a>Second page</a></Link></li>
+      <li><Link href="/page?page=page-3" as="/page-3"><a>Third page</a></Link></li>
+      <li><Link href="/page?page=page-5" as="/page-5"><a>404...</a></Link></li>
+      <li><Link href="/other"><a>Other page</a></Link></li>
+      <li><Link href="/about"><a>About page</a></Link></li>
+      <li><Link href="/contact"><a>Contact page</a></Link></li>
+    </ul>
+
+    <h1>{json.title}</h1>
+    <p>{json.content}</p>
+  </div>
+  )
 }
 
-export default withRouter(Index)
+Index.getInitialProps = async (o) => {
+  if (o.req) {
+    const data = require('../pages.json')
+    const json = data[o.asPath.slice(1)]
+    return { json }
+  }
+
+  return fetch(`/api/page${o.asPath}`)
+    .then((res) => res.json())
+    .then((json) => ({ json }))
+}
+
+export default Index
