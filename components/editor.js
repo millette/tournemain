bba220b284d1fa0fd833c35317ce8bbff7837d33
Robin Millette <robin@millette.info>
2018-12-04 08:30:45 -0500

    Replace draftjs with medium-draft.

--- components/editor.js
+++ components/editor.js
@@ -1,17 +1,37 @@
 // npm
 import { Component } from 'react'
-import { convertFromHTML, Editor, EditorState, ContentState } from 'draft-js'
+// import { convertFromHTML, Editor, EditorState, ContentState } from 'draft-js'
+// import { convertFromHTML, Editor, EditorState, ContentState } from 'medium-draft/dist/medium-draft'
+import { createEditorState, Editor, EditorState, ContentState } from 'medium-draft/dist/medium-draft'
+import mediumDraftExporter from 'medium-draft/dist/medium-draft-exporter'
+import mediumDraftImporter from 'medium-draft/dist/medium-draft-importer'
+import { convertToRaw } from 'draft-js'
+
+import 'medium-draft/dist/medium-draft.css'
+
 // TODO: evaluate https://github.com/HubSpot/draft-convert instead of draft-js-export-html
-import { stateToHTML } from 'draft-js-export-html'
+// import { stateToHTML } from 'draft-js-export-html'
+
+// import mediumDraftExporter from 'medium-draft/lib/exporter'
 
 class MyEditor extends Component {
   constructor(props) {
     super(props)
+
+    // const editorState = createEditorState(convertToRaw(mediumDraftImporter(props.initialContent)))
+
     this.state = { editorState: false, html: false }
     this.onChange = (editorState) => this.setState({ html: false, editorState })
 
+    /*
+    const editorState = // your draft editorState
+    const renderedHTML = mediumDraftExporter(editorState.getCurrentContent())
+    // Use renderedHTML
+    */
+
     this.showHTML = () => {
-      const html = stateToHTML(this.state.editorState.getCurrentContent())
+      // const html = stateToHTML(this.state.editorState.getCurrentContent())
+      const html = mediumDraftExporter(this.state.editorState.getCurrentContent())
       console.log('HTML:', html)
       console.log('HTML.length', html.length)
       this.setState({ html })
@@ -21,6 +41,10 @@ class MyEditor extends Component {
   componentDidMount() {
     // const it = ContentState.createFromText(this.props.initialContent)
 
+    const editorState = createEditorState(convertToRaw(mediumDraftImporter(this.props.initialContent)))
+    this.setState({ editorState })
+
+    /*
     const blocksFromHTML = convertFromHTML(this.props.initialContent)
     const it = ContentState.createFromBlockArray(
       blocksFromHTML.contentBlocks,
@@ -30,7 +54,17 @@ class MyEditor extends Component {
     this.setState({
       editorState: EditorState.createWithContent(it)
     })
+    */
   }
+
+  /*
+
+      <div className='md-RichEditor-root'>
+        <div className='public-DraftEditor-content' dangerouslySetInnerHTML={{ __html: this.props.initialContent }} />
+      </div>
+
+
+  */
 
   render() {
     return (
