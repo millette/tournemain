47471463b30f8200ed998dc7e64306f340d17f85
Robin Millette <robin@millette.info>
2018-12-05 06:30:40 -0500

    Give purgecss a try (fails).

--- next.config.js
+++ next.config.js
@@ -1,2 +1,25 @@
+// npm
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const PurgecssPlugin = require("purgecss-webpack-plugin")
+const glob = require("glob")
 const withCSS = require("@zeit/next-css")
-module.exports = withCSS()
+
+module.exports = withCSS({
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    if (dev || isServer) return config
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: "css-loader?sourceMap",
+      }),
+    })
+    config.plugins.push(
+      new ExtractTextPlugin("[name].css?[hash]"),
+      new PurgecssPlugin({
+        paths: glob.sync("*(components|pages)/*"),
+      }),
+    )
+    return config
+  },
+})
