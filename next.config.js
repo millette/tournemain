2f7862e22be92120e5285c13c2ba0a1d17b33e38
Robin Millette <robin@millette.info>
2018-12-07 08:22:17 -0500

    Purge css in prod only.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 // npm
 const withCSS = require("@zeit/next-css")
 const withPurgeCss = require("next-purgecss")
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
 
-module.exports = withCSS(
-  withPurgeCss({
-    purgeCss: { whitelistPatterns: () => [/^md-/] },
-  }),
-)
+const whitelistPatterns = () => [/^md-/]
+
+module.exports = (phase) => {
+  console.log("PHASE:", phase)
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return withCSS()
+  }
+  return withCSS(withPurgeCss({ purgeCss: { whitelistPatterns } }))
+}
