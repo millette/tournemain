a5b6e9859b20e950e39a9e2294ae08e801374d39
Robin Millette <robin@millette.info>
2018-12-07 17:22:12 -0500

    Fix purgecss config.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 // npm
 const withCSS = require("@zeit/next-css")
 const withPurgeCss = require("next-purgecss")
-const { PHASE_PRODUCTION_SERVER } = require("next/constants")
+const { PHASE_PRODUCTION_BUILD } = require("next/constants")
 
 const whitelistPatterns = () => [/^md-/]
 
 module.exports = (phase) => {
-  return phase === PHASE_PRODUCTION_SERVER
+  return phase === PHASE_PRODUCTION_BUILD
     ? withCSS(withPurgeCss({ purgeCss: { whitelistPatterns } }))
     : withCSS()
 }
