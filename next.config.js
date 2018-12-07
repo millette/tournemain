4f2e0a4fb60febb0570e2ee10995c8dc71840468
Robin Millette <robin@millette.info>
2018-12-07 09:14:11 -0500

    Remove failed attempt at bundle analyzer.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // npm
 const withCSS = require("@zeit/next-css")
 const withPurgeCss = require("next-purgecss")
-// const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
 const { PHASE_PRODUCTION_SERVER } = require("next/constants")
 
 const whitelistPatterns = () => [/^md-/]
@@ -10,23 +9,4 @@ module.exports = (phase) => {
   return phase === PHASE_PRODUCTION_SERVER
     ? withCSS(withPurgeCss({ purgeCss: { whitelistPatterns } }))
     : withCSS()
-  /*
-  const nextConfig = {
-      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-      bundleAnalyzerConfig: {
-        server: {
-          analyzerMode: 'static',
-          reportFilename: '../bundles/server.html'
-        },
-        browser: {
-          analyzerMode: 'static',
-          reportFilename: '../bundles/client.html'
-        }
-      },
-      webpack: (config) => config
-
-    }
-    return withBundleAnalyzer(nextConfig)
-  */
 }
