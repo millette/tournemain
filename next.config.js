0a19ffd370faedfa4efb1f9ff895f31429a2db97
Robin Millette <robin@millette.info>
2018-12-09 20:21:15 -0500

    Remove editor purgecss rules.

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const withCSS = require("@zeit/next-css")
 const withPurgeCss = require("next-purgecss")
 const { PHASE_PRODUCTION_BUILD } = require("next/constants")
 
-const whitelistPatterns = () => [/^md-/, /^public-Draft/, /^DraftEditor-/]
+// const whitelistPatterns = () => [/^md-/, /^public-Draft/, /^DraftEditor-/]
 
 module.exports = (phase) => {
   return phase === PHASE_PRODUCTION_BUILD
-    ? withCSS(withPurgeCss({ purgeCss: { whitelistPatterns } }))
+    ? // ? withCSS(withPurgeCss({ purgeCss: { whitelistPatterns } }))
+      withCSS(withPurgeCss())
     : withCSS()
 }
