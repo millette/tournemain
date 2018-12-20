91d4c1470d7d3c4a1268a17f73348ac01411ce6b
Robin Millette <robin@millette.info>
2018-12-19 21:33:15 -0500

    Fix tests.

--- test/main.js
+++ test/main.js
@@ -8,18 +8,35 @@ import srv from ".."
 // FIXME: we can only have one test
 //        since nextjs builds throws everything in .next
 test("el test#1", withPage, async (t, page) => {
-  const pages = {
-    "": {
-      title: "Tournemain",
-      content: '<p>bla bla <a href="/p2">Page 2</a></p>',
-    },
-    p2: {
-      title: "Tournemain - Page 2",
-      content: '<h2>boop</h2><p>bla bla <a href="/">Home</a></p>',
-    },
+  const docs = {
+    db: [
+      [
+        "",
+        {
+          _id: "",
+          title: "Tournemain",
+          content: '<p>bla bla <a href="/p2">Page 2</a></p>',
+          _rev: "0-dSf4VkAST4suKo6B0emBsg",
+          _created: 1545177746441,
+          _updated: 1545177746441,
+        },
+      ],
+      [
+        "p2",
+        {
+          _id: "p2",
+          title: "Tournemain - Page 2",
+          content: '<h2>boop</h2><p>bla bla <a href="/">Home</a></p>',
+          _rev: "0-abc4VkAST4suKo6B0emBsg",
+          _created: 1545177646441,
+          _updated: 1545177646441,
+        },
+      ],
+    ],
+    history: [["", []], ["p2", []]],
   }
 
-  const addr = await srv(pages)
+  const addr = await srv({ docs })
   await page.goto(`${addr}/other`)
 
   const abc = await page.$("section.section666")
@@ -27,6 +44,10 @@ test("el test#1", withPage, async (t, page) => {
 
   const abc2 = await page.$("section.section")
   t.truthy(abc2)
+
+  const abc2href = await page.$$("a")
+  t.truthy(abc2href)
+  t.is(abc2href.length, 1)
 
   await Promise.all([page.waitForNavigation(), page.click("a")])
 
