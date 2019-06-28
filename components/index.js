bfa17b56e536dc937744f1271cd1c533420050dc
Robin Millette <robin@millette.info>
2019-06-28 00:21:29 -0400

    Remove references to editor.

--- components/index.js
+++ components/index.js
@@ -1,7 +1,5 @@
 // self
 import Nav from "./nav"
-// import MyEditor from "./editor"
 import External from "./external"
 
-// export { Nav, MyEditor, External }
 export { Nav, External }
--- pages/page.js
+++ pages/page.js
@@ -4,11 +4,7 @@ import Error from "next/error"
 import Router from "next/router"
 import "isomorphic-unfetch"
 
-// import "medium-draft/dist/basic.css"
-// import "medium-draft/dist/medium-draft.css"
-
 // self
-// import { MyEditor, Nav } from "../components"
 import { Nav } from "../components"
 import { baseUrl } from "../utils"
 
@@ -31,44 +27,6 @@ export default class Page extends Component {
       )
     }
   }
-
-  /*
-  constructor(props) {
-    super(props)
-
-
-    this.state = { undo: false, edit: false, path: false }
-
-    this.edit = () => this.setState({ edit: true })
-
-    this.saveHTML = ({ html, path }) => {
-      fetch(`/api/page/${path}`, {
-        method: "PUT",
-        body: JSON.stringify({ html }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((response) => {
-          this.props.json.content = html
-          this.props.json.title = "Titre à venir"
-          delete this.props.json.statusCode
-          this.setState({ edit: false })
-        })
-        .catch(console.error)
-    }
-
-    this.cancelEdit = (ev) => {
-      let { undo, path } = ev
-      if (undo === "" || typeof undo !== "string") {
-        undo = false
-        path = false
-      }
-      this.setState({ undo, path, edit: false })
-    }
-  }
-  */
 
   static async getInitialProps(o) {
     return fetch(baseUrl(o.req, `api/page/${o.asPath.slice(1)}`))
